2a9a11e5ed8974e71bc331ae1833059640997a74
AndrejKorjakin <61660338+AndrejKorjakin@users.noreply.github.com>
2020-07-26 13:12:23 +0200

    Dodadena shoppingcart model i smeneti se pozadinite na site views

---
+++ BundleGames/BundleGames/Migrations/202007261043139_addedshoppingcart.cs
@@ -0,0 +1,16 @@
+namespace BundleGames.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedshoppingcart : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ BundleGames/BundleGames/Models/ShoppingCart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BundleGames.Models
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        public List<Game> GamesForBuying { get; set; }
+
+        public ShoppingCart()
+        {
+            GamesForBuying = new List<Game>();
+        }
+
+    }
+}
